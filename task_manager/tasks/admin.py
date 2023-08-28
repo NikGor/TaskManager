a079d68f6aaf8acf74d79ada367b311b439f7023
Nikolai <nicolas.gordienko@gmail.com>
2023-08-28 20:54:45 +0200

    feat: customized django admin pages

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Task
+from ..labels.models import Label
 
-admin.site.register(Task)
+
+class LabelInline(admin.StackedInline):
+    model = Label.tasks.through
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'status', 'project')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'description')
+    list_filter = ('status', 'project')
+    empty_value_display = '-пусто-'
+    list_editable = ('status',)
+    save_on_top = True
--- task_manager/users/admin.py
+++ task_manager/users/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Добавить дополнительные поля в list_display
+    list_display = ('username', 'email', 'first_name',
+                    'last_name', 'is_staff', 'is_active',
+                    'last_login', 'date_joined')
+
+    # Добавить фильтры
+    list_filter = ('is_staff', 'is_active')
+
+    # Поля только для чтения
+    readonly_fields = ('last_login', 'date_joined')
+
+    # Пользовательские действия
+    actions = ['make_active', 'make_inactive']
+
+    def make_active(self, request, queryset):
+        queryset.update(is_active=True)
+
+    make_active.short_description = "Активировать выбранных пользователей"
+
+    def make_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+
+    make_inactive.short_description = "Деактивировать выбранных пользователей"
