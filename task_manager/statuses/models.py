8d0aed79ae6f933402e2e6a63aea4b223ea97766
Nikolai <nicolas.gordienko@gmail.com>
2023-08-29 00:04:28 +0200

    fix: deleted views and templates for statuses

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Status(models.Model):
     name = models.CharField(max_length=200)
-    order = models.IntegerField()
+    order = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('users/', include('task_manager.users.urls')),
-    path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
     path('labels/', include('task_manager.labels.urls')),
     path('projects/', include('task_manager.projects.urls')),
