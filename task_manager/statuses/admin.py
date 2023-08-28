de3de150d75992ffb12863b583622906491a9b1b
Nikolai <nicolas.gordienko@gmail.com>
2023-08-28 23:32:14 +0200

    feat: add order to Status

--- task_manager/statuses/admin.py
+++ task_manager/statuses/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Status
 
+
 # Register your models here.
-admin.site.register(Status)
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'order', 'created_at')
+    list_display_links = ('id', 'name')
+    list_editable = ('order',)
--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Status(models.Model):
     name = models.CharField(max_length=200)
+    order = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
